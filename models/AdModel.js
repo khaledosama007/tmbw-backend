ff61d5b81a57ae60b38dd4ac41e329174def01ba
khaled <khaled@theplatform.me>
2020-05-09 00:29:38 +0200

    Adding pet object to Ad in getLatestAds

--- models/AdModel.js
+++ models/AdModel.js
@@ -1,13 +1,13 @@
 let monsgoose = require("mongoose");
 
 let AdSchema = monsgoose.Schema({
-    petId : {type : monsgoose.Schema.Types.ObjectId , required:true},
-    price : {type : Number , required : true},
-    address : {type: String , required : true},
-    phoneNumber : {type : String , required : true},
-    purpose : {type : String , required : true},
-    verified : {type : Boolean , required : true},
-    userId:{type : monsgoose.Schema.Types.ObjectId , required : true}
+  petId: { type: monsgoose.Schema.Types.ObjectId, required: true, ref: "Pet" },
+  price: { type: Number, required: true },
+  address: { type: String, required: true },
+  phoneNumber: { type: String, required: true },
+  purpose: { type: String, required: true },
+  verified: { type: Boolean, required: true },
+  userId: { type: monsgoose.Schema.Types.ObjectId, required: true },
 });
 
-module.exports = monsgoose.model("Ad" , AdSchema);
+module.exports = monsgoose.model("Ad", AdSchema);
