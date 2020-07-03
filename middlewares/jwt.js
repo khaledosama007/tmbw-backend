d75ae415f8059ec2a36d72a79c12d4adc75794fe
khaled <khaledosama500@gmail.com>
2020-07-03 21:58:53 +0200

    fixing JWT bug

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -3,6 +3,7 @@ const secret = process.env.JWT_SECRET;
 
 const authenticate = jwt({
   secret: secret,
+  algorithms: ["RS256"],
   credentialsRequired: false,
 }).unless({ path: ["api/v1/auth/register/", "api/v1/auth/login/"] });
 
